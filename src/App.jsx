81571c75316e25b39a08d757c2b31ca6efb9b308
burgosuc <jlb1983@gmail.com>
2023-11-17 17:06:03 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { PizzasContextProvider } from './context/PizzasContext'
+import Navbar from './components/Navbar'
+import Home from './views/Home'
+import DetailsPizza from './views/DetailsPizza'
+import Checkout from './views/Checkout'
+import 'bootstrap/dist/css/bootstrap.min.css'; import NotFound from './views/NotFound'
+
+function App () {
+  return (
+    <div className='App'>
+      <PizzasContextProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/Home' element={<Home />} />
+            <Route path='/pizza/:id' element={<DetailsPizza />} />
+            <Route path='/carrito' element={<Checkout />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </PizzasContextProvider>
+    </div>
+  )
+}
+
+export default App
